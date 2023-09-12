d96def716cf9af7dbf717d589b09e8e7979ae935
Graham Smith <gsmitheidw@gmail.com>
2023-09-12 17:03:52 +0100

    basic package framework

---
+++ yed-nojre/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.yworks.com/resources/yed/demo/yEd-3.23.2.zip' 
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  softwareName  = 'yed-nojre*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = '4F96611718DF696DE2F33EEB2CD78BFBBDACCE52390AFEA8D00441B6FB175E20'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs 
+
+
